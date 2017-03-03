d9002e35424609d09bd0e659688558f56c0203ed
Helio Alves <helios25@gmail.com>
2017-03-02 21:15:46 -0500

    add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,24 +1,60 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-const User = mongoose.model('User', {
-  firstname: {
-    type: String,
-    required: true,
-    minlength: 2,
-    trim: true
-  },
-  lastname: {
-    type: String,
-    required: true,
-    minlength: 2,
-    trim: true
-  },
+const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     minlength: 5,
-    trim: true
-  }
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
+
+//Overwriting what the server sends in the response
+// we only wan to send the _id and email
+UserSchema.methods.toJSON = function() {
+  const user = this;
+  const userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+// We are generating the token that will be used to access some sections of the site
+UserSchema.methods.generateAuthToken = function() {
+  const user = this;
+  const access = 'auth';
+  const token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc').toString();
+
+  user.tokens.push({ access, token });
+
+  return user.save()
+    .then(() => {
+      return token;
+    });
+};
+
+const User = mongoose.model('User', UserSchema);
 
 module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -4,12 +4,20 @@ const { ObjectID } = require('mongodb');
 const app = require('express')();
 const bodyParser = require('body-parser');
 const _ = require('lodash');
+const jwt = require('jsonwebtoken');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
 app.use(bodyParser.json());
+
+
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                               Todos routes                                  *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
 
 // POST /todos
 app.post('/todos', (req, res) => {
@@ -99,6 +107,29 @@ app.patch('/todos/:id', (req, res) => {
       res.send({ todo });
     })
     .catch((err) => res.status(404).send());
+});
+
+/*
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*                               User routes                                   *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*/
+
+// POST /users
+app.post('/users', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+  const user = new User(body);
+
+  user.save()
+    .then(() =>{
+      return user.generateAuthToken()
+    })
+    .then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
 });
 
 app.listen(process.env.PORT, () => {
