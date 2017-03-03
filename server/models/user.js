f24edfd4c0cb0c919c714f6e213c225dda31146b
Helio Alves <helios25@gmail.com>
2017-03-02 22:31:17 -0500

    add GET /users/me route and authentication

--- server/models/user.js
+++ server/models/user.js
@@ -32,6 +32,12 @@ const UserSchema = new mongoose.Schema({
   }]
 });
 
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                              Model Methods                                  *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
 //Overwriting what the server sends in the response
 // we only wan to send the _id and email
 UserSchema.methods.toJSON = function() {
@@ -45,7 +51,7 @@ UserSchema.methods.toJSON = function() {
 UserSchema.methods.generateAuthToken = function() {
   const user = this;
   const access = 'auth';
-  const token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc').toString();
+  const token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
 
   user.tokens.push({ access, token });
 
@@ -53,6 +59,32 @@ UserSchema.methods.generateAuthToken = function() {
     .then(() => {
       return token;
     });
+};
+
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                             Instance Methods                                *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
+UserSchema.statics.findByToken = function(token) {
+  let User = this;
+  let decoded = undefined;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch( err) {
+    return Promise.reject();
+    /*return new Promise((resolve, reject) => {
+      reject();
+    });*/
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
 };
 
 const User = mongoose.model('User', UserSchema);
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const jwt = require('jsonwebtoken');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
+const { authenticate } = require('./middleware/authenticate');
 
 app.use(bodyParser.json());
 
@@ -130,6 +131,10 @@ app.post('/users', (req, res) => {
     .catch((err) => {
       res.status(400).send(err);
     });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(process.env.PORT, () => {
