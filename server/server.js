178edc92ef48fd25d0905fb03c55d145f40fa3aa
Helio Alves <helios25@gmail.com>
2017-03-14 16:31:11 -0400

    added route DELETE /users/me/token/

--- server/server.js
+++ server/server.js
@@ -158,7 +158,7 @@ app.post('/users/login', (req, res) => {
 });
 
 // POST /users/logout
-app.delete('/users/me/logout', authenticate, (req, res) => {
+app.delete('/users/me/token', authenticate, (req, res) => {
   req.user.removeToken(req.token)
     .then(() => {
       res.status(200).send();
