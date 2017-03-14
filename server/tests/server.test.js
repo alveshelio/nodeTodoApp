b96a77f4f24f0f226a12c9292944ee9f0b11423d
Helio Alves <helios25@gmail.com>
2017-03-14 14:12:16 -0400

    add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -235,6 +235,9 @@ describe('POST /users', () => {
             expect(user).toExist();
             expect(user.password).toNotBe(password);
             done();
+          })
+          .catch((err) => {
+            done(err);
           });
       });
   });
@@ -266,4 +269,59 @@ describe('POST /users', () => {
       })
       .end(done);
   });
+});
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    const email = users[1].email;
+    const password = users[1].password;
+    request(app)
+      .post('/users/login')
+      .send({ email, password })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist()
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id)
+          .then((user) => {
+            expect(user.tokens[1]).toInclude({
+              access: 'auth',
+              token: res.headers['x-auth']
+            });
+            done();
+          })
+          .catch((err) => {
+            done(err);
+          });
+      });
+  });
+
+  it('should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({ email: 'abc@abc.com', password: 'abc' })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist()
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id)
+          .then((user) => {
+            expect(user.tokens[1]).toNotExist();
+            done();
+          })
+          .catch((err) => {
+            done(err);
+          });
+      });
+  }); // /it
 });
